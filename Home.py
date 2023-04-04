63e8ac7de254fb248d4e8b6139910955ca46f389
cheaton622 <84142940+cheaton622@users.noreply.github.com>
2023-04-04 10:39:58 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,7 +13,7 @@ import requests
 
 st.set_page_config(page_title="MLS",page_icon="🏚️",layout='wide',initial_sidebar_state='collapsed')
 
-url = "https://github.com/cheaton622/MLS-Price-Predictor/blob/main/Streamlit.csv" # Make sure the url is the raw version of the file on GitHub
+url = "https://github.com/cheaton622/MLS-Price-Predictor/blob/main/Streamlit.xlsx" # Make sure the url is the raw version of the file on GitHub
 
 # Reading the downloaded content and turning it into a pandas dataframe
 
