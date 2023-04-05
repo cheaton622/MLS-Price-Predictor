fa40ea8de6e1642ad071d29cdeeee7fb9351c579
cheaton622 <84142940+cheaton622@users.noreply.github.com>
2023-04-05 07:38:38 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ from sklearn.impute import SimpleImputer
 import requests
 import openpyxl
 
-st.set_page_config(page_title="MLS",page_icon="🏚️",layout='wide',initial_sidebar_state='collapsed')
+st.set_page_config(page_title="MLS",page_icon="🏚️",layout='wide',initial_sidebar_state='expanded')
 # Create the Streamlit app
 st.title("Home Sale Price Predictor")
 url = "https://raw.githubusercontent.com/cheaton622/MLS-Price-Predictor/main/Streamlit.csv" # Make sure the url is the raw version of the file on GitHub
